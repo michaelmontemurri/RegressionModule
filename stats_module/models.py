166ff170f21e28d256a838f5b48c88cc136528c8
Michael Montemurri <michaelmontemurri@mail.mcgill.ca>
2024-11-23 13:32:48 -0500

    OLS implementation (kind of)

--- stats_module/models.py
+++ stats_module/models.py
@@ -3,23 +3,23 @@ import pandas as pd
 from sklearn import NotFittedError
 from sklearn.preprocessing import OneHotEncoder
 
-class LinearRegression:
+class OLS:
     def __init__(self, include_intercept=True):
         self.include_intercept = include_intercept
         self.beta = None
 
+    #should we make use_gradient_descent a parameter automatically use it if p>threshold?
     def fit(self, X, y):
         if y.ndim > 1:
             raise ValueError("y must be a 1-dimensional array.")
-
-        # handle categorical data by converting categorical columns into dummy variables
-        if isinstance(X, pd.DataFrame):
-            enc = OneHotEncoder(drop='first')
-            #onehot encode the categorical columns
-            enc.fit(X.select_dtypes(include='object'))
-            X = pd.concat([X.drop(enc.get_feature_names(), axis=1), 
-                           pd.DataFrame(enc.transform(X.select_dtypes(include='object')).toarray(), 
-                                        columns=enc.get_feature_names())], axis=1)
+        
+        #raise error if X and y have different number of observations
+        if X.shape[0] != y.shape[0]:
+            raise ValueError("X and y must have the same number of observations.")
+        
+        #raise error if X has categorical variables
+        if isinstance(X, pd.DataFrame) and X.select_dtypes(include='object').shape[1] > 0:
+            raise ValueError("X cannot have categorical variables.")
             
         if self.include_intercept:
             X_ = np.column_stack([np.ones(X.shape[0]), X])
@@ -34,18 +34,10 @@ class LinearRegression:
                           "check for highly correlated features.")
         
         
-
     def predict(self, X):
         if self.beta is None:
-            raise NotFittedError("This LinearRegression instance is not fitted yet. "
+            raise NotFittedError("This OLS instance is not fitted yet. "
                                    "Call 'fit' with appropriate data before using this estimator.")
-
-        if isinstance(X, pd.DataFrame):
-            enc = OneHotEncoder(drop='first')
-            enc.fit(X.select_dtypes(include='object'))
-            X = pd.concat([X.drop(enc.get_feature_names(), axis=1), 
-                           pd.DataFrame(enc.transform(X.select_dtypes(include='object')).toarray(), 
-                                        columns=enc.get_feature_names())], axis=1)
         if self.include_intercept:
             X_ = np.column_stack([np.ones(X.shape[0]), X])
         else:
