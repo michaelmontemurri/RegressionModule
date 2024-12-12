c25063e8c78763bca1832cdd34689d6c165797b9
Evelyn <19eah7@queensu.ca>
2024-12-12 16:25:05 -0500

    finalized inference stuff and gulliver's stuff

--- stats_module/__init__.py
+++ stats_module/__init__.py
@@ -1,11 +1,11 @@
-#from .loss_estimation import naive_loss_estimation, train_test_loss_estimation, loo_loss_estimation
+from .loss_estimation import naive_loss_estimation, train_test_loss_estimation, loo_loss_estimation
 from .models import OLS, GLS
 from .testing import LinearModelTester
 from .utils import validate_data
 
-__all__ = [#'naive_loss_estimation',
-            #'train_test_loss_estimation',
-            #'loo_loss_estimation',
+__all__ = ['naive_loss_estimation',
+            'train_test_loss_estimation',
+            'loo_loss_estimation',
             'OLS',
             'GLS',
             'LinearModelTester',
